6135769fbc5f6dec7ba588594dcc96bcb383ebcd
aliyarerbolatov12-maker <aliyarerbolatov12@gmail.com>
2025-10-01 17:58:02 +0500

    update(js): new UnionFindBasic implementation

--- union_find/js/union_find_basic.js
+++ union_find/js/union_find_basic.js
@@ -1,9 +1,24 @@
 class UnionFindBasic {
-  arrayId = [];
+  ids = [];
 
   constructor(n) {
     for (let i = 0; i < n; i++) {
-      this.arrayId[i] = i;
+      this.ids[i] = i;
+    }
+  }
+
+  connected(a, b) {
+    return this.ids[a] === this.ids[b];
+  }
+
+  union(a, b) {
+    const idA = this.ids[a];
+    const idB = this.ids[b];
+
+    for (let i = 0; i < this.ids.length; i++) {
+      if (this.ids[i] === idA) {
+        this.ids[i] = idB;
+      }
     }
   }
 }
